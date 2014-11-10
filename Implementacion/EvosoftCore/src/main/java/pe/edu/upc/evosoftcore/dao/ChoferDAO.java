0ab6d4ae881f736d5a368b53b4d61ae780cd0d58
jccanales <carloscm.inm@gmail.com>
2014-11-09 22:59:23 -0500

    Creacion de la interfaz e implementacion DAO

---
+++ Implementacion/EvosoftCore/src/main/java/pe/edu/upc/evosoftcore/dao/ChoferDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.evosoftcore.dao;
+
+import java.util.List;
+import pe.edu.upc.evosoftcore.entity.Chofer;
+
+/**
+ *
+ * @author Jean Carlo
+ */
+public interface ChoferDAO {
+    
+    public void insert(Chofer chofer);
+    public Chofer findByChoferId(int choferId);
+    public List<Chofer> listChofer();
+    
+}
