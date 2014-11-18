abced8f98417b0d2002276622b7c508b926f2516
jccanales <carloscm.inm@gmail.com>
2014-11-17 23:20:09 -0500

    Actualizacion POM y ChoferTest

--- Implementacion/EvosoftCore/src/test/java/pe/edu/upc/evosoftcore/test/ChoferTest.java
+++ Implementacion/EvosoftCore/src/test/java/pe/edu/upc/evosoftcore/test/ChoferTest.java
@@ -5,80 +5,45 @@
  */
 package pe.edu.upc.evosoftcore.test;
 
-import java.util.List;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-import pe.edu.upc.evosoftcore.dao.ChoferDAO;
-import pe.edu.upc.evosoftcore.entity.Chofer;
-import pe.edu.upc.evosoftcore.entity.Licencia;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pe.edu.upc.evosoftcore.core.service.ChoferService;
+import pe.edu.upc.evosoftcore.core.service.LicenciaService;
+import pe.edu.upc.evosoftcore.domain.Chofer;
+import pe.edu.upc.evosoftcore.domain.Licencia;
 
 /**
  *
  * @author Jean Carlo
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/java//pe/edu/upc/evosoftcore/configuration/SpringContext.xml"})
 public class ChoferTest {
 
-    private Chofer chofer;
-    private Licencia licencia;
-    private ApplicationContext context;
-    private ChoferDAO choferDAO;
-
-    public ChoferTest() {
-
-        context = new ClassPathXmlApplicationContext("Spring-Module.xml");
-        choferDAO = (ChoferDAO) context.getBean("ChoferDAO");
-
-        chofer = new Chofer();
-        chofer.setNombre("Marco");
-        chofer.setApellido("Polo");
-        chofer.setTelefono(1234567);
-
-        licencia = new Licencia();
-        licencia.setIdLicencia(1);
-        chofer.setLicencia(licencia);
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Test(priority = 1)
-    public void InsertarTest() {
-        System.out.println("======INSERTARTEST======");
-        choferDAO.insert(chofer);
-        System.out.println("Inserte: " + chofer.toString());
-        ListarChofer();
-
-    }
-
-    @Test(priority = 2)
-    public void UpdateTest() {
-        System.out.println("======UPDATETEST======");
-        chofer.setNombre("marco update");
-        chofer.setApellido("apapapapaptest");
-        chofer.setTelefono(9874563);
-        choferDAO.update(chofer);
-        ListarChofer();
-    }
+    @Autowired
+    private ChoferService choferService;
+    @Autowired
+    private LicenciaService licenciaService;
     
-    @Test(priority = 3)
-    public void DeleteTest(){
-        System.out.println("======DELETETEST======");
-        choferDAO.delete(chofer);
-        ListarChofer();
-    }
+    @Test
+    public void Insertar() {
+        try {
+            Licencia l = licenciaService.getRepository().findOne(1);
+            Chofer c = new Chofer();
+            c.setNombre("Chofer");
+            c.setApellido("Spring");
+            c.setTelefono(888888);
+            c.setIdLicencia(l);
+            choferService.getRepository().save(c);
+            System.out.println("Cantidad de choferes: " + choferService.getRepository().metodoPersonalizado());
 
-    public void ListarChofer() {
-        List<Chofer> choferes = choferDAO.listar();
-        for (Chofer c : choferes) {
-            System.out.println(c.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
 }
