41c9e940cb910b46581d5acde38e0dedc19c707d
diego2608 <diegoif26@gmail.com>
2014-11-23 22:57:10 -0500

    Implementacion de LicenciaTest

--- Implementacion/EvosoftCore/src/test/java/pe/edu/upc/evosoftcore/test/LicenciaTest.java
+++ Implementacion/EvosoftCore/src/test/java/pe/edu/upc/evosoftcore/test/LicenciaTest.java
@@ -3,13 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pe.edu.upc.evosoftcore.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pe.edu.upc.evosoftcore.core.service.LicenciaService;
+import pe.edu.upc.evosoftcore.domain.Licencia;
 
 /**
  *
  * @author Diego
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/java//pe/edu/upc/evosoftcore/configuration/SpringContext.xml"})
+
 public class LicenciaTest {
-    
+
+    @Autowired
+    private LicenciaService licenciaService;
+
+    @Test
+    public void Insertar() {
+        try {
+            Licencia l = licenciaService.getRepository().findOne(2);
+            l.setCategoria(null);
+            l.setFechaVencimiento(null);
+            l.setNumero(Integer.SIZE);
+            
+            licenciaService.getRepository().save(l);
+            System.out.println("Cantidad de licencias" + licenciaService.getRepository().metodoPersonalizado());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
